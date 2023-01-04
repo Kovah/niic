ea6e634bb13f4e96545d6378ee4c23848ed313a4
Kovah <mail@kovah.de>
2023-01-04 14:05:43 +0100

    Fix image loading for items

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -6,8 +6,8 @@ import Stats from './Stats.vue';</script>
         <div class="mb-4 border border-gray-900 w-20 h-20 cursor-pointer rounded"
             v-on:click="openItemSelector">
             <div class="border border-gray-700 w-full h-full rounded flex items-center justify-center">
-                <img :src="itemImagePath" :alt="item.name" v-if="this.itemLoaded">
-                <span class="p-1 text-xs text-gray-500" v-if="!this.itemLoaded">Select an Item</span>
+                <img :src="itemImagePath" :alt="item.name" v-if="itemLoaded">
+                <span class="p-1 text-xs text-gray-500" v-if="!itemLoaded">Select an Item</span>
             </div>
         </div>
         <Stats :item="item"/>

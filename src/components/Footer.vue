83e9f77eb48ed494b7e1319103b94bc8c9b595d1
Kovah <mail@kovah.de>
2023-01-05 00:04:21 +0100

    Add more sets, correct stats for existing items, use numbers in item ids

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,9 @@
 <template>
     <footer class="my-5  text-gray-500 text-xs">
-        <p>The NIIC (NGU Idle Item Comparator) lets you compare two items from the game <a href="https://store.steampowered.com/app/1147690/NGU_IDLE/" rel="noreferrer nofollow" class="underline hover:text-gray-300">NGU Idle</a>.</p>
+        <p>
+            The NIIC (NGU Idle Item Comparator) lets you compare two items from the game <a href="https://store.steampowered.com/app/1147690/NGU_IDLE/" rel="noreferrer nofollow" class="underline hover:text-gray-300">NGU Idle</a>.<br>
+            The item stats are calculated based on their maximum bonus when upgraded to level 100.
+        </p>
         <div class="mt-2 flex justify-between">
             <div class="mr-3">
                 &copy; {{ year }} <a href="https://kovah.de" class="underline hover:text-gray-300">Kevin Woblick</a>

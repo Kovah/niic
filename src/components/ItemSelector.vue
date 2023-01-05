4f1216647038414c476a5be8d842ddde703e7b7c
Kovah <mail@kovah.de>
2023-01-05 12:32:51 +0100

    Allow filter by set

--- src/components/ItemSelector.vue
+++ src/components/ItemSelector.vue
@@ -7,14 +7,32 @@
                     <h2 class="h2 mb-4">Item Select</h2>
 
                     <div class="text-xs text-gray-400 mb-1">Filter Items</div>
-                    <div class="mb-4 space-x-1">
-                        <button class="btn" v-on:click="toggleFilter('head', $event)">Head</button>
-                        <button class="btn" v-on:click="toggleFilter('chest', $event)">Chest</button>
-                        <button class="btn" v-on:click="toggleFilter('legs', $event)">Legs</button>
-                        <button class="btn" v-on:click="toggleFilter('boots', $event)">Boots</button>
-                        <button class="btn" v-on:click="toggleFilter('weapon', $event)">Weapon</button>
-                        <button class="btn" v-on:click="toggleFilter('accessory', $event)">Accessory</button>
+                    <div class="flex mb-4">
+                        <div>
+                            <button class="btn" v-on:click="toggleFilter('head', $event)">Head</button>
+                            <button class="btn" v-on:click="toggleFilter('chest', $event)">Chest</button>
+                            <button class="btn" v-on:click="toggleFilter('legs', $event)">Legs</button>
+                            <button class="btn" v-on:click="toggleFilter('boots', $event)">Boots</button>
+                            <button class="btn" v-on:click="toggleFilter('weapon', $event)">Weapon</button>
+                            <button class="btn" v-on:click="toggleFilter('accessory', $event)">Accessory</button>
+                        </div>
+                        <div>
+                            <select class="select" name="set" id="set" v-on:change="filterBySet">
+                                <option value="">Select Set</option>
+                                <option value="2D">2D</option>
+                                <option value="Cave">Cave</option>
+                                <option value="Clock">Clock</option>
+                                <option value="Forest">Forest</option>
+                                <option value="GRB">GRB</option>
+                                <option value="HSB">HSB</option>
+                                <option value="Jake">Jake</option>
+                                <option value="Sewers">Sewers</option>
+                                <option value="Spoopy">Spoopy</option>
+                                <option value="Training">Training</option>
+                            </select>
+                        </div>
                     </div>
+
 
                     <div class="flex flex-wrap">
                         <div class="w-12 h-12 border-2 border-gray-700 text-xs break-words cursor-pointer"
@@ -40,25 +58,34 @@ export default {
     data () {
         return {
             store,
-            appliedFilters: []
+            typeFilters: [],
+            setFilter: null
         };
     },
     computed: {
         itemList () {
-            if (this.appliedFilters.length > 0) {
-                return itemData.filter((item) => this.appliedFilters.includes(item.type));
+            let items = itemData;
+            if (this.typeFilters.length > 0) {
+                items = items.filter((item) => this.typeFilters.includes(item.type));
             }
-            return itemData;
+            if (this.setFilter !== null) {
+                items = items.filter((item) => item.set === this.setFilter);
+            }
+            return items;
         }
     },
     methods: {
         toggleFilter (newFilter, event) {
-            if (!this.appliedFilters.includes(newFilter)) {
-                this.appliedFilters.push(newFilter);
+            if (!this.typeFilters.includes(newFilter)) {
+                this.typeFilters.push(newFilter);
             } else {
-                this.appliedFilters = this.appliedFilters.filter((existingFilter) => existingFilter !== newFilter);
+                this.typeFilters = this.typeFilters.filter((existingFilter) => existingFilter !== newFilter);
             }
             event.target.classList.toggle('btn-active');
+        },
+        filterBySet (event) {
+            const newSet = event.target.options[event.target.selectedIndex].value;
+            this.setFilter = newSet !== '' ? newSet : null;
         },
         applyItem (item) {
             store.setItem(item);

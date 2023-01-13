24d2e5980b805f5b1369eac9ff99c2247d000873
Kovah <mail@kovah.de>
2023-01-13 09:40:29 +0100

    Update readme, add preview

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
-# NIIC - NGU Idle Item Comparator
+<h1 align="center">NIIC - NGU Idle Item Comparator</h1>
+
+<p>&nbsp;</p>
+
+<img src="/preview.png" alt="Browser screenshot as a preview of the tool" align="center">
+
+<p>&nbsp;</p>
+
+## About
 
 The NIIC (NGU Idle Item Comparator) lets you compare two items from the game NGU Idle. Select two items from the item selector and the comparator will show you the statistics for both items side-by-side.
-
+The item stats are calculated based on their maximum bonus when upgraded to level 100. Select two items for both slots and the comparator will show you which stats are better. In the selector you can filter by item type (head, chest, weapon,..), by set, or search by name.
 
 ## Development
 
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,7 +2,7 @@
     <footer class="my-5 px-3 text-gray-500 text-xs">
         <p>
             The NIIC (NGU Idle Item Comparator) lets you compare two items from the game <a href="https://store.steampowered.com/app/1147690/NGU_IDLE/" rel="noreferrer nofollow" class="underline hover:text-gray-300">NGU Idle</a>.<br>
-            The item stats are calculated based on their maximum bonus when upgraded to level 100.
+            The item stats are calculated based on their maximum bonus when upgraded to level 100. Select two items for both slots and the comparator will show you which stats are better. In the selector you can filter by item type (head, chest, weapon,..), by set, or search by name.
         </p>
         <div class="mt-2 flex justify-between">
             <div class="mr-3">
